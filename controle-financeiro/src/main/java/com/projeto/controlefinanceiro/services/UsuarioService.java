5fffd21313a2e7e7503faf1234138a41858b8a7d
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-07-19 20:41:12 -0300

    :sparkles: feat: Nova classe de serviços

---
+++ controle-financeiro/src/main/java/com/projeto/controlefinanceiro/services/UsuarioService.java
@@ -0,0 +1,13 @@
+package com.projeto.controlefinanceiro.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto.controlefinanceiro.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+}

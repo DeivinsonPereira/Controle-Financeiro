f9952112058e0992d3f6d3497dff56b8f43b67e1
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-07-28 21:07:57 -0300

    :wrench: chore : atualização spring

--- controle-financeiro/src/main/java/com/projeto/controlefinanceiro/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.controlefinanceiro.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.controlefinanceiro.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
--- controle-financeiro/src/main/java/com/projeto/controlefinanceiro/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.controlefinanceiro.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.controlefinanceiro.entities.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}

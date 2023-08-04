eaee234b7ac078a2e8745b669fb85d92cdf8f3c8
vtsuellen <vtsuellen@hotmail.com>
2023-08-04 16:02:18 -0300

    Initial Commit

---
+++ front-end/src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-6xl font-bold text-center">Iniciando Projeto</h1>
+    </main>
+  )
+}
